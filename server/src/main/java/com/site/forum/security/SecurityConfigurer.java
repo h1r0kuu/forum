154744f6c1b33116834987215d61118c48c53265
glapi <abhazim.nurbat@gmail.com>
2022-06-08 17:20:07 +0200

    user security

---
+++ server/src/main/java/com/site/forum/security/SecurityConfigurer.java
@@ -0,0 +1,37 @@
+package com.site.forum.security;
+
+import com.site.forum.service.impl.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    private final UserServiceImpl userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf()
+            .disable()
+            .authorizeRequests()
+            .anyRequest().permitAll();
+    }
+}
