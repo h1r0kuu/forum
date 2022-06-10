a3baa0b1bed494d3002f06c1f75b2fc828cd5702
glapi <abhazim.nurbat@gmail.com>
2022-06-10 20:08:02 +0200

    Header

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,7 +1,18 @@
+import "../styles/header.css"
 
 function Header() {
     return (
-        <></>
+        <header>
+            <ul>
+                <li>Logo</li>
+                <li>Home</li>
+                <li>Faq</li>
+            </ul>
+            <div className="search">
+                <input type="text" className="search-input"/>
+                <button>Search</button> 
+            </div>
+        </header>
     )
 }
 
