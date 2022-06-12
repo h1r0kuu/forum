2862413dc425a1dee1d4f5c916cf3b4b35bef75a
glapi <abhazim.nurbat@gmail.com>
2022-06-12 22:04:58 +0200

    getAll method

--- client/src/services/PostService.js
+++ client/src/services/PostService.js
@@ -4,6 +4,10 @@ class PostSevice {
     getPostsByForumId(forumId) {
         return axios.get('http://localhost:8080/api/v1/posts/forum/' + forumId)
     }
+
+    getAll() {
+        return axios.get('http://localhost:8080/api/v1/posts/all')
+    }
 }
 
 export default new PostSevice()
