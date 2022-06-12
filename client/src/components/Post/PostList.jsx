9c3642411982149ea3021c12e0ab0909401d2afe
glapi <abhazim.nurbat@gmail.com>
2022-06-12 22:04:08 +0200

    update

--- client/src/components/Post/PostList.jsx
+++ client/src/components/Post/PostList.jsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react";
 import {useParams} from "react-router-dom";
 import PostService from "../../services/PostService";
+import "../../styles/post-list.css"
+import Post from "./Post";
 
 function PostList() {
     const {forumId} = useParams()
     const [posts, setPosts] = useState([])
 
   useEffect(()=>{
-    PostService.getPostsByForumId(7).then(res => {
+    if(forumId === undefined || forumId === null) {
+      PostService.getAll().then(res => {
+          setPosts(res.data)
+      })
+    } else {
+      PostService.getPostsByForumId(forumId).then(res => {
         setPosts(res.data)
-    })  
+      })  
+    }
   }, [])
 
   return (
       <div className="post-list">
-
         {posts.map(post => (
-            //TODO: add Post component
-            <div className="post" key={post.id}>
-                <p>{post.title} - {post.createdAt}</p>
-            </div>
+            <Post post={post} />
         ))}
       </div>
   )
