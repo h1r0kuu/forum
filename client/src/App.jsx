e9f274edad79d65c874be5a3c5d0bfd5740ba99e
glapi <abhazim.nurbat@gmail.com>
2022-06-14 22:18:11 +0200

    Routes update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import PostCreate from "./layouts/PostCreate";
 import Index from "./pages/Index";
+import SinglePost from "./pages/SinglePost";
 import "./styles/app.css"
-import ForumPosts from "./pages/ForumPosts";
 
 function App() {
   return (
     <>
-      <Router>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/forums/:forumId" element={<ForumPosts />} />
-          </Routes>
-      </Router>
+      <Routes>
+        <Route path="*" element={<Index />} />
+        <Route exact path="/forums/:forumId" element={<Index />} />
+        <Route exact path="/forums/:forumId/posts/create" element={<PostCreate />} />
+        <Route exact path="/posts/:postId" element={<SinglePost />} />
+      </Routes>
     </>
   )  
 }
