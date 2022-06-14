6ee974ee4224eae9657892d43d9278e6832c8b66
glapi <abhazim.nurbat@gmail.com>
2022-06-14 22:18:44 +0200

    classname

--- client/src/layouts/ForumList.jsx
+++ client/src/layouts/ForumList.jsx
@@ -1,25 +1,23 @@
 import {useState, useEffect} from 'react'
-import { Link } from 'react-router-dom'
+import {Link, Route, Routes, useLocation } from 'react-router-dom'
 import ForumService from '../services/ForumService'
 import "../styles/forum-list.css"
 
 function ForumList() {
-    const [forums, setForums] = useState([])
-
+  const [forums, setForums] = useState([])
   useEffect(()=>{
     ForumService.getAllForums().then( res => {
       setForums(res.data)
-      console.log(res.data)
     })
-    }, [])
+  }, [])
 
-    return (
-        <div className="forums">
-            {forums.map(forum=> (
-                <p key={forum.id}><Link to={`/forums/${forum.id}`} onClick={() => window.location.href(`/forums/${forum.id}`)}>{forum.title}</Link></p>
-            ))}
-        </div>
-    )
+  return (
+      <div className="forums">
+        {forums.map(forum=> ( 
+          <p className="forum-link" key={forum.id}><Link to={`/forums/${forum.id}`}>{forum.title}</Link></p>
+        ))}
+      </div>
+  )
 }
 
 export default ForumList
