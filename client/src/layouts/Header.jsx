1c62ce95c6183d8ed2b471590a0b3b8eed1afd5b
glapi <abhazim.nurbat@gmail.com>
2022-06-14 22:19:21 +0200

    Modal

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,18 +1,28 @@
+import { Link } from "react-router-dom"
+import ForumChoose from "./ForumChoose"
 import "../styles/header.css"
+import { useState } from "react"
 
 function Header() {
+    const [openModal, setModal] = useState(false)
+
     return (
+        <>
         <header>
             <ul>
                 <li>Logo</li>
-                <li>Home</li>
+                <li><Link to={'/'}>Home</Link></li>
                 <li>Faq</li>
+                <li><a href="#" onClick={()=>setModal(true)}>Create post</a></li>
+                
             </ul>
             <div className="search">
                 <input type="text" className="search-input"/>
                 <button>Search</button> 
             </div>
         </header>
+        {openModal && <ForumChoose openModal={openModal} setModal={setModal} />}
+        </>
     )
 }
 
