d50b399ca083b3afe05256d1a7da43e87e59f281
glapi <abhazim.nurbat@gmail.com>
2022-06-14 22:19:46 +0200

    create, getone methods

--- client/src/services/PostService.js
+++ client/src/services/PostService.js
@@ -1,12 +1,23 @@
 import axios from "axios"
-
+const POST_API_RUL = "http://localhost:8080/api/v1/posts"
 class PostSevice {
+    getOne(postId) {
+        return axios.get(`${POST_API_RUL}/` + postId)
+    }
+
     getPostsByForumId(forumId) {
-        return axios.get('http://localhost:8080/api/v1/posts/forum/' + forumId)
+        return axios.get(`${POST_API_RUL}/forum/` + forumId)
     }
 
     getAll() {
-        return axios.get('http://localhost:8080/api/v1/posts/all')
+        return axios.get(`${POST_API_RUL}/all`)
+    }
+
+    create(data, callback) {
+        axios.post(`${POST_API_RUL}/create`, data)
+        .then((res) => {
+            callback(res)
+        })
     }
 }
 
