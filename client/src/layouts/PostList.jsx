f1b89e986386464594630acaddbb37ff8f31b043
glapi <abhazim.nurbat@gmail.com>
2022-06-15 20:18:04 +0200

    infinity scroll

--- client/src/layouts/PostList.jsx
+++ client/src/layouts/PostList.jsx
@@ -3,28 +3,48 @@ import {useParams} from "react-router-dom";
 import PostService from "../services/PostService";
 import "../styles/post-list.css"
 import Post from "../components/Post/Post";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function PostList() {
-    const {forumId} = useParams()
-    const [posts, setPosts] = useState([])
+  const {forumId} = useParams()
+  const [posts, setPosts] = useState([])
+  const [hasMore, setHasMore] = useState(true);
+  let page = 0;
 
-  useEffect(()=>{
+  const loadMorePosts = () => {
     if(forumId === undefined || forumId === null) {
-      PostService.getAll().then(res => {
-          setPosts(res.data)
+      PostService.getAll(page).then(res => {
+        const newPosts = res.data.content
+        setPosts((oldPosts) => [...oldPosts, ...newPosts])
+
+        if(newPosts.length === 0 || newPosts.length < 10) {
+          setHasMore(false)
+        }
       })
     } else {
       PostService.getPostsByForumId(forumId).then(res => {
         setPosts(res.data)
       })  
     }
+    page += 1
+  }
+
+  useEffect(()=>{
+    loadMorePosts()
   }, [forumId])
 
   return (
       <div className="post-list">
-        {posts.map(post => (
-            <Post post={post} key={post.id} />
-        ))}
+        <InfiniteScroll
+          dataLength={posts.length}
+          next={loadMorePosts}
+          hasMore={hasMore}
+          loader={<h4>Loading</h4>}
+        >
+        {posts.map( post => {
+          return <Post post={post} key={post.id} />;
+        })}
+        </InfiniteScroll>
       </div>
   )
 }
