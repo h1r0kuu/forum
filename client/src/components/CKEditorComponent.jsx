a62ac5fd233e371c16cec9898b40bd7e1ea8d077
glapi <abhazim.nurbat@gmail.com>
2022-06-15 20:17:05 +0200

    fix

--- client/src/components/CKEditorComponent.jsx
+++ client/src/components/CKEditorComponent.jsx
@@ -2,32 +2,58 @@ import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "ckeditor5-custom-build";
 import { Component } from "react"
 
-
-
-
-class CKEditorInput extends Component {
-    render() {
-        return (
-            <CKEditor
-                editor= {ClassicEditor}
-                // config={ editorConfiguration }
-                onReady={ editor => {
-                    // You can store the "editor" and use when it is needed.
-                    console.log( 'Editor is ready to use!', editor );
-                } }
-                onChange={ ( event, editor ) => {
-                    const data = editor.getData();
-                    console.log( { event, editor, data } );
-                } }
-                onBlur={ ( event, editor ) => {
-                    console.log( 'Blur.', editor );
-                } }
-                onFocus={ ( event, editor ) => {
-                    console.log( 'Focus.', editor );
-                } }
-            />
-        )
+const editorConfiguration = {
+    // toolbar: {
+    //     items: [
+    //         'heading',
+    //         '|',
+    //         'bold',
+    //         'italic',
+    //         'underline',
+    //         'strikethrough',
+    //         'subscript',
+    //         'superscript',
+    //         '|',
+    //         'bulletedList',
+    //         'numberedList',
+    //         'todoList',
+    //         '|',
+    //         'alignment',
+    //         'outdent',
+    //         'indent',
+    //     ]
+    // }
+    ckfinder: {
+        uploadUrl: "/api/v1/images/upload",
+        headers: {
+          'X-CSRF-TOKEN': 'CSFR-Token',
+          Authorization: 'Bearer <JSON Web Token>'
+        }
     }
+};
+
+
+function CKEditorInput({postText, setPostText}) {
+    return (
+        <CKEditor
+            editor= {ClassicEditor}
+            config={ editorConfiguration }
+            onReady={ editor => {
+                // You can store the "editor" and use when it is needed.
+                console.log( 'Editor is ready to use!', editor );
+            } }
+            onChange={ (event, editor ) => {
+                const data = editor.getData();
+                setPostText(data)
+            } }
+            onBlur={ ( event, editor ) => {
+                console.log(editor.getData())
+            } }
+            onFocus={ ( event, editor ) => {
+            } }
+        />
+    )
+    // }
 }
 
 export default CKEditorInput
