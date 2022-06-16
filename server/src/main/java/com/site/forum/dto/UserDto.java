7b263b3a3cd531bac7d9caf07e6d9ccdcf2593f5
glapi <abhazim.nurbat@gmail.com>
2022-06-16 22:08:13 +0200

    User profile picture & comments count

--- server/src/main/java/com/site/forum/dto/UserDto.java
+++ server/src/main/java/com/site/forum/dto/UserDto.java
@@ -2,10 +2,12 @@ package com.site.forum.dto;
 
 import com.site.forum.entity.User;
 import com.site.forum.enums.UserRole;
+import com.site.forum.model.UserModel;
 import lombok.Data;
 import org.modelmapper.ModelMapper;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Data
 public class UserDto {
@@ -13,16 +15,38 @@ public class UserDto {
     private String username;
     private String password;
     private UserRole role;
+    private String imagePath;
+    private int commentsCount;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     public UserDto convertToDto(User user) {
         ModelMapper modelMapper = new ModelMapper();
-        return modelMapper.map(user, UserDto.class);
+        UserDto userDto = modelMapper.map(user, UserDto.class);
+        userDto.setCommentsCount(  Objects.nonNull(user.getComments()) ? user.getComments().size() : 0 );
+        return userDto;
+    }
+
+    public UserDto modelToDto(UserModel model) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(model, UserDto.class);
     }
 
     public User convertToEntity(UserDto userDto) {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper.map(userDto, User.class);
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                ", commentsCount=" + commentsCount +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
     }
 }
