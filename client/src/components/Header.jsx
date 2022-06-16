2c54db0f6002901d1823895402e61065e05f0bfd
glapi <abhazim.nurbat@gmail.com>
2022-06-16 22:10:28 +0200

    fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import Login from "./Auth/Login"
 import {observer} from 'mobx-react-lite'
 import "../styles/header.css"
+import Registration from "./Auth/Registration"
 
 function Header({store}) {
     const [openModal, setModal] = useState(false)
@@ -14,21 +15,25 @@ function Header({store}) {
                 <li>Logo</li>
                 <li><Link to={'/'}>Home</Link></li>
                 <li>Faq</li>
-                <li><a href="#" onClick={()=>setModal(true)}>Create post</a></li>
-                
+                {store.isAuth &&
+                    <li><a href="#" onClick={()=>setModal(true)}>Create post</a></li>
+                }
             </ul>
             <div className="search">
                 <input type="text" className="search-input" />
                 <button>Search</button> 
             </div>
-            {store.isAuth ? 
+            {store.isAuth ?
+                <> 
+                    <Link to={"/user/" + store.user.username}>Profile</Link>
                 <div>
                     <a href="#" onClick={()=> store.logout()}>Logout</a>
                 </div>
+                </>
                 :
                 <>
                     <Link to={"/login"} element={<Login />}>Login</Link>
-                    <Link to={"/registration"}>Reg</Link>
+                    <Link to={"/registration"} element={<Registration />}>Reg</Link>
                 </>
             }
         </header>
