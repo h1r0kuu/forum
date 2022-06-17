067b2d5bbd0523d3a2377f8ca3b551ea53ef8998
glapi <abhazim.nurbat@gmail.com>
2022-06-17 20:48:30 +0200

    Admin routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,13 @@ import { Context } from "./index";
 import { observer } from 'mobx-react-lite'
 import "./styles/app.css"
 import Profile from "./components/User/Profile"
+import PostList from "./components/Post/PostList"
+import Admin from "./pages/Admin/AdminIndex"
+import AdminForums from "./pages/Admin/AdminForums"
+import AdminPosts from "./pages/Admin/AdminPosts"
+import AdminUsers from "./pages/Admin/AdminUsers"
+import AdminIndex from "./pages/Admin/AdminIndex"
+import AdminForumCreate from "./pages/Admin/AdminForumCreate"
 
 function App() {
   const {store} = useContext(Context);
@@ -23,7 +30,7 @@ function App() {
     <>
       <Routes>
         <Route path="*" element={<Index store={store} />} />
-        <Route exact path="/forums/:forumId" element={<Index store={store}/>} />
+        <Route exact path="/forums/:forumId" element={<PostList store={store}/>} />
         <Route exact path="/forums/:forumId/posts/create" element={<PostCreate store={store} />} />
         <Route exact path="/posts/:postId" element={<SinglePost store={store}/>} />
 
@@ -32,6 +39,15 @@ function App() {
 
         <Route exact path="/user/:username" element={<Profile store={store} />} />
 
+        {store.user.role === "ADMIN" &&
+          <>
+            <Route exact path="/admin" element={<AdminIndex store={store} />} />
+            <Route exact path="/admin/forums" element={<AdminForums store={store} />} />
+            <Route exact path="/admin/posts" element={<AdminPosts store={store} />} />
+            <Route exact path="/admin/users" element={<AdminUsers store={store} />} />
+            <Route exact path="/admin/forums/create" element={<AdminForumCreate store={store} />} />
+          </>
+        }
       </Routes>
     </>
   )  
---
+++ server/src/main/java/com/site/forum/model/ForumModel.java
@@ -0,0 +1,9 @@
+package com.site.forum.model;
+
+import lombok.Data;
+
+@Data
+public class ForumModel {
+    private String title;
+    private Long parentId;
+}
