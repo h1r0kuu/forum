14378ace1e76d71808026ca850ee85d0adbb3ad8
glapi <abhazim.nurbat@gmail.com>
2022-06-18 22:11:56 +0200

    Forum request model for forum creating

--- server/src/main/java/com/site/forum/model/ForumModel.java
+++ server/src/main/java/com/site/forum/model/ForumModel.java
@@ -1,9 +1,11 @@
 package com.site.forum.model;
 
 import lombok.Data;
+import org.springframework.lang.Nullable;
 
 @Data
 public class ForumModel {
     private String title;
+    @Nullable
     private Long parentId;
 }
