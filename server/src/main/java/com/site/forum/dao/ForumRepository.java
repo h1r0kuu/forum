1d5404abd50a6b4c8a4c54256a817afd5b879693
glapi <abhazim.nurbat@gmail.com>
2022-06-18 22:12:22 +0200

    Check if forum is a sub forum

--- server/src/main/java/com/site/forum/dao/ForumRepository.java
+++ server/src/main/java/com/site/forum/dao/ForumRepository.java
@@ -2,6 +2,7 @@ package com.site.forum.dao;
 
 import com.site.forum.entity.Forum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,8 @@ import java.util.List;
 @Repository
 public interface ForumRepository extends JpaRepository<Forum, Long> {
     List<Forum> findByCreator_Username(String username);
+    @Query("SELECT CASE WHEN COUNT(f) > 0 THEN true ELSE false END FROM Forum f" +
+            " join f.subForums res" +
+            " ON res.id = ?1")
+    Boolean isForumASubForum(Long forumId);
 }
