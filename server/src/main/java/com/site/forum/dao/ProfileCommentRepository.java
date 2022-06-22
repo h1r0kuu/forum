be6cfa38ce7673bb73fda8cee82dbbadefedbbf9
glapi <abhazim.nurbat@gmail.com>
2022-06-22 21:55:43 +0200

    Add repository annotation

--- server/src/main/java/com/site/forum/dao/ProfileCommentRepository.java
+++ server/src/main/java/com/site/forum/dao/ProfileCommentRepository.java
@@ -2,9 +2,11 @@ package com.site.forum.dao;
 
 import com.site.forum.entity.ProfileComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProfileCommentRepository extends JpaRepository<ProfileComment, Long> {
     List<ProfileComment> findByCommentator_Username(String username);
     List<ProfileComment> findByUser_Username(String username);
