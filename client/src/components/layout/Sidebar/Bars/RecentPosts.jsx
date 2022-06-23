3a229ff93ffcf8f906dc0342de8ca4e910fac17c
glapi <abhazim.nurbat@gmail.com>
2022-06-23 22:17:30 +0200

    Get 3 recent posts

--- client/src/components/layout/Sidebar/Bars/RecentPosts.jsx
+++ client/src/components/layout/Sidebar/Bars/RecentPosts.jsx
@@ -1,30 +1,30 @@
+import Moment from 'react-moment';
+import { useEffect, useState } from "react"
+import PostService from "../../../../services/PostService"
+
 function RecentPosts() {
+    const [posts, setPosts] = useState([])
+    useEffect(()=>{
+        PostService.getAll(0, 'createdAt').then(res => {
+            setPosts(res.data.content.slice(0,3))
+        })
+    }, [])
+    console.log(posts)
+
     return (
         <div className="recent-post3290">
             <h4>Recent Post</h4>
-            <div className="post-details021">
+            {posts.map(post=>(
+                <>
+                <div className="post-details021">
                 <a href="#">
-                    <h5>How much do web developers</h5>
+                    <h5>{post.title}</h5>
                 </a>
-                <p>I am thinking of pursuing web developing as a career &amp; was ...</p>
-                <small style={{color: "#848991"}}>July 16, 2017</small>
+                <small style={{color: "#848991"}}><Moment fromNow>{post.createdAt}</Moment></small>
             </div>
-            <hr/>
-            <div className="post-details021">
-                <a href="#">
-                    <h5>How much do web developers</h5>
-                </a>
-                <p>I am thinking of pursuing web developing as a career &amp; was ...</p>
-                <small style={{color: "#848991"}}>July 16, 2017</small>
-            </div>
-            <hr/>
-            <div className="post-details021">
-                <a href="#">
-                    <h5>How much do web developers</h5>
-                </a>
-                <p>I am thinking of pursuing web developing as a career &amp; was ...</p>
-                <small style={{color: "#848991"}}>July 16, 2017</small>
-            </div>
+            <hr/> 
+                </>
+            ))}
         </div>
     )
 }
