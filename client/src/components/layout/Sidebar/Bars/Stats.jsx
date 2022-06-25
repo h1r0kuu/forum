07edc2b9b492819d80a864b519d4aeaec15bf2c9
glapi <abhazim.nurbat@gmail.com>
2022-06-25 22:19:09 +0200

    Statistics

--- client/src/components/layout/Sidebar/Bars/Stats.jsx
+++ client/src/components/layout/Sidebar/Bars/Stats.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react"
+import StatsService from "../../../../services/StatsService"
+
 function Stats() {
+    const [stats, setStats] = useState({})
+
+    useEffect(() => {
+        StatsService.stats().then(res => {
+            setStats(res.data)
+        })
+    }, [])
+
+
     return (
         <div className="status-part3821">
             <h4>stats</h4>
             <a href="#">
-                <i className="fa fa-question-circle" aria-hidden="true"> Question(20)</i>
+                <i className="fa fa-question-circle" aria-hidden="true"> Users({stats.users})</i>
             </a>
-            <i className="fa fa-comment" aria-hidden="true"> Answers(50)</i>
+            <i className="fa fa-comment" aria-hidden="true"> Posts({stats.posts})</i>
         </div>
     )
 }
