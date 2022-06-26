6796aba5c291415f47f678d1b5319f74f3cd384d
glapi <abhazim.nurbat@gmail.com>
2022-06-26 22:15:43 +0200

    Comment replies

--- client/src/components/layout/Sidebar/Sidebar.jsx
+++ client/src/components/layout/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import Stats from "./Bars/Stats"
 import LoginBar from "./Bars/LoginBar"
 import RecentPosts from "./Bars/RecentPosts"
+import OnlineUsers from "./Bars/OnlineUsers"
 
 function Sidebar({store}) {
     return (
@@ -9,108 +10,7 @@ function Sidebar({store}) {
             {!store.isAuth &&
                 <LoginBar />
             }
-            {/* <div className="highest-part302">
-                <h4>Highest Points</h4>
-                <div className="pints-wrapper">
-                <div className="left-user3898">
-                    <a href="#">
-                    <img src="image/images.png" alt="Image"/>
-                    </a>
-                    <div className="imag-overlay39">
-                    <a href="#">
-                        <i className="fa fa-plus" aria-hidden="true"></i>
-                    </a>
-                    </div>
-                </div>
-                <span className="points-details938">
-                    <a href="#">
-                    <h5>Ahmed Hasan</h5>
-                    </a>
-                    <a href="#" className="designetion439">Pundit</a>
-                    <p>206 points</p>
-                </span>
-                </div>
-                <hr/>
-                <div className="pints-wrapper">
-                <div className="left-user3898">
-                    <a href="#">
-                    <img src="image/images.png" alt="Image"/>
-                    </a>
-                    <div className="imag-overlay39">
-                    <a href="#">
-                        <i className="fa fa-plus" aria-hidden="true"></i>
-                    </a>
-                    </div>
-                </div>
-                <span className="points-details938">
-                    <a href="#">
-                    <h5>Ahmed Hasan</h5>
-                    </a>
-                    <a href="#" className="designetion439">Pundit</a>
-                    <p>206 points</p>
-                </span>
-                </div>
-                <hr/>
-                <div className="pints-wrapper">
-                <div className="left-user3898">
-                    <a href="#">
-                    <img src="image/images.png" alt="Image"/>
-                    </a>
-                    <div className="imag-overlay39">
-                    <a href="#">
-                        <i className="fa fa-plus" aria-hidden="true"></i>
-                    </a>
-                    </div>
-                </div>
-                <span className="points-details938">
-                    <a href="#">
-                    <h5>Ahmed Hasan</h5>
-                    </a>
-                    <a href="#" className="designetion439">Pundit</a>
-                    <p>206 points</p>
-                </span>
-                </div>
-                <hr/>
-                <div className="pints-wrapper">
-                <div className="left-user3898">
-                    <a href="#">
-                    <img src="image/images.png" alt="Image"/>
-                    </a>
-                    <div className="imag-overlay39">
-                    <a href="#">
-                        <i className="fa fa-plus" aria-hidden="true"></i>
-                    </a>
-                    </div>
-                </div>
-                <span className="points-details938">
-                    <a href="#">
-                    <h5>Ahmed Hasan</h5>
-                    </a>
-                    <a href="#" className="designetion439">Pundit</a>
-                    <p>206 points</p>
-                </span>
-                </div>
-                <hr/>
-                <div className="pints-wrapper">
-                <div className="left-user3898">
-                    <a href="#">
-                    <img src="image/images.png" alt="Image"/>
-                    </a>
-                    <div className="imag-overlay39">
-                    <a href="#">
-                        <i className="fa fa-plus" aria-hidden="true"></i>
-                    </a>
-                    </div>
-                </div>
-                <span className="points-details938">
-                    <a href="#">
-                    <h5>Ahmed Hasan</h5>
-                    </a>
-                    <a href="#" className="designetion439">Pundit</a>
-                    <p>206 points</p>
-                </span>
-                </div>
-            </div> */}
+            <OnlineUsers />
             <RecentPosts />
         </aside>
     )
