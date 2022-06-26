e9f601ec9665ee88e87c385d553b3b8245118ed1
glapi <abhazim.nurbat@gmail.com>
2022-06-26 22:21:06 +0200

    Get all posts that are not hidden for the user

--- server/src/main/java/com/site/forum/dao/PostRepository.java
+++ server/src/main/java/com/site/forum/dao/PostRepository.java
@@ -13,6 +13,9 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Page<Post> findAll(Pageable pageable);
+    @Query("SELECT p FROM Post p" +
+            " WHERE p.id NOT IN (SELECT h.id FROM User u JOIN u.hiddenPosts h WHERE u.username = ?1)")
+    Page<Post> findAll(Pageable pageable, String username);
     List<Post> findByForum_Id(Long id);
     Page<Post> findByForum_Id(Long id, Pageable pageable);
     List<Post> findByCreator_Username(String username);
