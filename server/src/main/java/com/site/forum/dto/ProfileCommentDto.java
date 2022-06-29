d9ef67a5265642dcc3c9b1094985edcfe7383b1b
glapi <abhazim.nurbat@gmail.com>
2022-06-29 20:42:42 +0200

    Validation

--- server/src/main/java/com/site/forum/dto/ProfileCommentDto.java
+++ server/src/main/java/com/site/forum/dto/ProfileCommentDto.java
@@ -1,14 +1,13 @@
 package com.site.forum.dto;
 
-import com.site.forum.entity.Forum;
 import com.site.forum.entity.ProfileComment;
-import com.site.forum.model.ForumModel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
@@ -16,8 +15,11 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 public class ProfileCommentDto {
     private Long id;
+    @NotNull(message = "Commentator cannot be null")
     private UserDto commentator;
+    @NotNull(message = "User cannot be null")
     private UserDto user;
+    @Size(min = 3, max = 255, message = "The text must be between 3 and 255 characters long")
     private String text;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
