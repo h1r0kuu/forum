e51c141003f4f9701efd5dcf599f1dd8d84f2e6d
glapi <abhazim.nurbat@gmail.com>
2022-06-29 20:40:38 +0200

    Validations

--- server/src/main/java/com/site/forum/model/FollowModel.java
+++ server/src/main/java/com/site/forum/model/FollowModel.java
@@ -2,8 +2,12 @@ package com.site.forum.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class FollowModel {
+    @NotBlank(message = "Following username cannot be empty")
     private String followingUsername;
+    @NotBlank(message = "Follower username cannot be empty")
     private String followerUsername;
 }
