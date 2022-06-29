810797b075224247487407a8da6ac8528960122e
glapi <abhazim.nurbat@gmail.com>
2022-06-29 20:34:35 +0200

    Rename header from Authentication to Authorization

--- client/src/utils/authQuery.js
+++ client/src/utils/authQuery.js
@@ -8,7 +8,7 @@ const api = axios.create({
 })
 
 api.interceptors.request.use((config) => {
-    config.headers.Authentication = `Bearer ${localStorage.getItem('token')}`
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
     return config;
 })
 
