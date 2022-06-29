4acd33c34dba78647187832bea497b4f226c25cf
glapi <abhazim.nurbat@gmail.com>
2022-06-29 20:40:21 +0200

    NotNull

--- server/src/main/java/com/site/forum/model/AuthModel.java
+++ server/src/main/java/com/site/forum/model/AuthModel.java
@@ -1,11 +1,14 @@
 package com.site.forum.model;
 
+import com.sun.istack.NotNull;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
 public class AuthModel {
+    @NotNull
     private String username;
+    @NotNull
     private String password;
 }
