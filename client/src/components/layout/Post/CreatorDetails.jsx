efff6b665a5dede45318bf5c252da321ae3d2177
glapi <abhazim.nurbat@gmail.com>
2022-07-10 21:05:42 +0200

    Rename

--- client/src/components/layout/Post/CreatorDetails.jsx
+++ client/src/components/layout/Post/CreatorDetails.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom"
 
-function AuthorDetails({author}) {
+function CreatorDetails({creator}) {
     return (
         <div className="author-details8392">
             <div className="author-img202l">
-                <img src="image/images.png" alt="image"/>
+                <img src={creator.imagePath} alt="image"/>
                 <div className="au-image-overlay text-center">
                     <a href="#">
                         <i className="fa fa-plus-square-o" aria-hidden="true"></i>
@@ -12,12 +12,10 @@ function AuthorDetails({author}) {
                 </div>
             </div>
             <span className="author-deatila04re">
-                <h5>About the Author <Link to={"/user/"+author.username}>{author.username}</Link></h5>
-                
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra auctor neque. Nullam lobortis, sapien vitae lobortis tristique.</p>
+                <h5>About the Author <Link to={"/user/"+creator.username}>{creator.username}</Link></h5>
             </span>
         </div>
     )
 }
 
-export default AuthorDetails
+export default CreatorDetails
