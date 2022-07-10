dd7172ba0f3c8e44bc6de25aad46562ee968fc7e
glapi <abhazim.nurbat@gmail.com>
2022-07-10 21:06:26 +0200

    Redesign

--- client/src/components/Comments/UserComment.jsx
+++ client/src/components/Comments/UserComment.jsx
@@ -1,6 +1,7 @@
 import "../../assets/css/user-comments.css"
 
 import { Link } from "react-router-dom"
+import Moment from "react-moment"
 
 function UserComment({comment}) {
     return (
@@ -14,20 +15,14 @@ function UserComment({comment}) {
                         <Link to={"/user/" + comment.user.username}>{comment.user.username}</Link>
                     </h6>
                     <span>
-                        <i className="fa fa-clock-o"> January 15 , 2014 at 10:00 pm</i>
+                        <i className="fa fa-clock-o"> <Moment fromNow>{comment.createdAt}</Moment></i>
                     </span>
                 </div>
                 <div className="comment-content"> {comment.text} </div>
-                <div className="l-rightside39">
-                    <button type="button" className="tolltip-button thumbs-up2" data-toggle="tooltip" data-placement="bottom" title="Like">
-                        {comment.likesCount} <i className="fa fa-thumbs-o-up " aria-hidden="true"></i>
-                    </button>
-                    <button type="button" className="tolltip-button  thumbs-down2" data-toggle="tooltip" data-placement="bottom" title="Dislike">
-                        {comment.dislikesCount} <i className="fa fa-thumbs-o-down" aria-hidden="true"></i>
-                    </button>
-                    <span className="single-question-vote-result">{comment.likesCount - comment.dislikesCount}</span>
+                <div className="stats">
+                    <span className="single-question-vote-result"></span>
                     <i className="fa fa-reply"> {comment.replies.length}</i>
-                    <i className="fa fa-heart" aria-hidden="true"> {comment.likesCount}</i>
+                    <i className="fa fa-heart" aria-hidden="true"> {comment.likesCount - comment.dislikesCount}</i>
                 </div>
             </div>
         </div>
