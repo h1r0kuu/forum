aa755399db9b216bf65477bcbbc5b6382c117926
glapi <abhazim.nurbat@gmail.com>
2022-07-19 22:22:39 +0200

    Recent posts

--- client/src/components/Sidebars/RecentPosts/RecentPosts.jsx
+++ client/src/components/Sidebars/RecentPosts/RecentPosts.jsx
@@ -2,19 +2,48 @@ import "./RecentPostsStyles.css"
 
 import { Link } from "react-router-dom"
 import dayjs from "../../../utils/dayjsRelative"
+import { useEffect, useState } from "react"
+import { PostService } from "../../../services/postService"
+import { MakeUrl } from "../../../utils/urls"
+import { extractContent } from "../../../utils/textUtil"
 
 
 function RecentPosts() {
+    const [posts, setPosts] = useState([])
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(() => {
+        PostService.getRecentPosts().then(data => {
+            setPosts(data)
+            setLoading(false)
+        })
+    }, [])
+
     return (
         <div className="recent-posts">
             <h4>Recent Post</h4>
-            <div className="post-details">
-                <Link to={"#"}>
-                    <h5>How much do web developers</h5>
-                </Link>
-                <p>I am thinking of pursuing web developing as a career & was ...</p> 
-                <small style={{color: "#848991"}}>{dayjs("2022-07-15 17:49:39.982076").fromNow(true)}</small>
-            </div>
+            {isLoading ? (
+                <p>Loading</p>
+            ) : (
+                posts.map(post => {
+                    const postText = extractContent(post.text)
+                    return (
+                        <div className="post-details" key={post.id}>
+                            <Link to={MakeUrl.postUrl(post.id)}>
+                                <h5>{post.title.slice(0,30)}</h5>
+                            </Link>
+                            <p>
+                                {
+                                    postText.length > 60 ?
+                                    postText.slice(0, 60) + "..." :
+                                    postText.slice(0, 60)
+                                }
+                            </p> 
+                            <small style={{color: "#848991"}}>{dayjs(post.createdAt).fromNow(true)}</small>
+                        </div>
+                    )
+                })
+            )}
         </div>
     )
 }
