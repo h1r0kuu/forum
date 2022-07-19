22d21c241f36e594efcc1830649722bd5665f590
glapi <abhazim.nurbat@gmail.com>
2022-07-19 22:20:13 +0200

    update

--- client/src/components/Sidebars/OnlineUsers/OnlineUsers.jsx
+++ client/src/components/Sidebars/OnlineUsers/OnlineUsers.jsx
@@ -18,34 +18,38 @@ function OnlineUsers() {
 
 
     return (
-        <div className="online-users">
-            <h4>Online users</h4>
-            {isLoading ? (
-                <p>Loading</p>
-            ) : (
-                users.map(user => (
-                    <div className="pints-wrapper" key={user.id}>
-                        <div className="user">
-                        <Link to={"#"}>
-                            <img src={user.imagePath} alt={user.username}/>
-                        </Link>
-                        <div className="user-img">
+        <>
+        {users.length > 0 &&
+            <div className="online-users">
+                <h4>Online users</h4>
+                {isLoading ? (
+                    <p>Loading</p>
+                ) : (
+                    users.map(user => (
+                        <div className="pints-wrapper" key={user.id}>
+                            <div className="user">
                             <Link to={"#"}>
-                                <i className="fa fa-plus" aria-hidden="true"></i>
+                                <img src={user.imagePath} alt={user.username}/>
                             </Link>
+                            <div className="user-img">
+                                <Link to={"#"}>
+                                    <i className="fa fa-plus" aria-hidden="true"></i>
+                                </Link>
+                            </div>
+                            </div>
+                            <span className="online">
+                                <Link to={MakeUrl.userUrl(user.username)}>
+                                    <h5>{user.username}</h5>
+                                </Link>
+                                <Link to={"#"} className="onlide-design">Online</Link>
+                            </span>
                         </div>
-                        </div>
-                        <span className="online">
-                            <Link to={MakeUrl.userUrl(user.username)}>
-                                <h5>{user.username}</h5>
-                            </Link>
-                            <Link to={"#"} className="onlide-design">Online</Link>
-                        </span>
-                    </div>
-                ))
-            )}
-            <hr/>
-        </div>
+                    ))
+                )}
+                <hr/>
+            </div>
+        }
+        </>
     )
 }
 
