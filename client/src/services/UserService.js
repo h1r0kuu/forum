2e49ef1656fce9a60cdd252ec201b5b776c3b387
glapi <abhazim.nurbat@gmail.com>
2022-07-20 21:57:59 +0200

    unfollow user

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -2,41 +2,54 @@ import axios from "axios"
 
 import { USER_API_URL } from "../utils/urls"
 
-class UserService {
+export const UserService = {
     async getAll() {
         const {data} = await axios.get(`${USER_API_URL}/all`)
         return data
-    }
+    },
 
     async getUser(username) {
         const {data} = await axios.get(`${USER_API_URL}/${username}`)
         return data
-    }
+    },
 
     async follow(payload) {
         const {data} = await axios.post(`${USER_API_URL}/follow`, payload)
         return data
-    }
+    },
+
+    async unfollow(payload) {
+        const {data} = await axios.post(`${USER_API_URL}/unfollow`, payload)
+        return data
+    },
     
     async getUserPosts(username, page=0, order="createdAt") {
         const {data} = await axios.get(`${USER_API_URL}/${username}/posts?page=${page}&order=${order}`)
         return data
-    }
+    },
 
     async getUserComments(username) {
         const {data} = await axios.get(`${USER_API_URL}/${username}/comments`)
         return data
-    }
+    },
+
+    async getUserFollowingList(username) {
+        const {data} = await axios.get(`${USER_API_URL}/${username}/following`)
+        return data
+    },
+
+    async getUserFollowers(username) {
+        const {data} = await axios.get(`${USER_API_URL}/${username}/followers`)
+        return data
+    },
 
     async getOnlineUsers() {
         const {data} = await axios.get(`${USER_API_URL}/online`)
         return data
-    }
+    },
 
     async getUserHiddenPosts(username) {
         const {data} = await axios.get(`${USER_API_URL}/${username}/hidden_posts`)
         return data
-    }
+    },
 }
-
-export default new UserService()
