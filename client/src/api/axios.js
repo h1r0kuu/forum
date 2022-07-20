eed7660a4c508f2fc8688135d6b611cb92decbe3
glapi <abhazim.nurbat@gmail.com>
2022-07-20 22:02:55 +0200

    axios fix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -15,16 +15,23 @@ axios.interceptors.request.use((config) => {
 axios.interceptors.response.use((config) => {
     return config;
 },async (error) => {
-    const originalRequest = error.config
-    if (error.response.status === 401 && error.config && !error.config._isRetry) {
+    let originalRequest = error.config
+    if(error.config && !error.config._isRetry) {
         originalRequest._isRetry = true
-        try {
-            const response = await axios.get(`${API_URL}/auth/refresh?token=${localStorage.getItem("token")}`)
-            localStorage.setItem('token', response.data.token)
+        if (error.response.status === 401 && localStorage.getItem("token") !== null) {
+            console.log("HERE")
+            try {
+                const response = await axios.get(`${API_URL}/auth/refresh?token=${localStorage.getItem("token")}`)
+                localStorage.setItem('token', response.data.token)
+                return axios.request(originalRequest)
+            } catch (e) {
+                console.log(e)
+            }
+        } else if(error.response.status === 500) {
+            delete originalRequest.headers['Authorization']
+            localStorage.removeItem('token')
             return axios.request(originalRequest)
-        } catch (e) {
-            console.log(e)
-        }
+        } 
     }
     throw error
 })
