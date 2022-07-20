d87e61783aa7053317672d3d8235b45a086f53aa
glapi <abhazim.nurbat@gmail.com>
2022-07-20 22:00:14 +0200

    Post list fix

--- client/src/components/PostList/PostList.jsx
+++ client/src/components/PostList/PostList.jsx
@@ -7,24 +7,7 @@ import { useSearchParams } from "react-router-dom"
 
 import {PostService} from "../../services/postService"
 
-function PostList({page, order, direction}) {
-    const [posts, setPosts] = useState([])
-    const [pagination, setPagination] = useState({})
-    const [hasMore, setHasMore] = useState(true);
-    const [isLoading, setLoading] = useState(true)
-
-    const loadMorePosts = useCallback(() => {
-        PostService.getAll(page,order, direction).then(data => {
-            const {content, ...pagin} = data
-            setPosts(content)
-            setPagination(pagin)
-            setLoading(false)
-        })
-    }, [page, order, direction])
-
-    useEffect(()=>{
-        loadMorePosts()
-    }, [loadMorePosts])
+function PostList({isLoading, posts, pagination,page, order, direction, forum, additionalParams}) {
     
     return (
         <section className="post-list">
@@ -36,7 +19,7 @@ function PostList({page, order, direction}) {
                         <PostListItem post={post} key={post.id}/>
                     ))}
                     {pagination.totalPages > 1 &&
-                        <Pagination pagination={pagination} order={order}/>
+                        <Pagination pagination={pagination} order={order} additionalParams={additionalParams}/>
                     }
                 </>
             )}
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -10,14 +10,36 @@ import Sidebars from "../../components/Sidebars/Sidebars";
 import { Link, useSearchParams } from "react-router-dom";
 import { DIRECTION, ORDER } from "../../constants/orderConstants";
 import { MakeUrl } from "../../utils/urls";
+import { useCallback, useEffect, useState } from "react";
+import { PostService } from "../../services/postService";
 
 function Home() {
+
+    const [posts, setPosts] = useState([])
+    const [pagination, setPagination] = useState({})
+    const [hasMore, setHasMore] = useState(true);
+    const [isLoading, setLoading] = useState(true)
+
     const [searchParams] = useSearchParams();
     
     const order = searchParams.get("order") || ORDER.CREATED_AT
     const page = searchParams.get("page") || 0
     const direction = searchParams.get("direction") || DIRECTION.DESC
-    
+    const forum = searchParams.get("forum") || null
+
+    const loadMorePosts = useCallback(() => {
+        PostService.getAll(page, order, direction, forum).then(data => {
+            const {content, ...pagin} = data
+            setPosts(content)
+            setPagination(pagin)
+            setLoading(false)
+        })
+    }, [page, order, direction])
+
+    useEffect(()=>{
+        loadMorePosts()
+    }, [loadMorePosts])
+
     function isActive(value) {
         if(order === value) {
             return "active"
@@ -53,7 +75,15 @@ function Home() {
                                     className={"order-param " + (isActive(ORDER.WITHOUT_COMMENTS))}>
                                     Without comments
                                 </Link> */}
-                                <PostList page={page} order={order} direction={direction}/>
+                                <PostList
+                                    page={page}
+                                    order={order}
+                                    direction={direction}
+                                    forum={forum}
+                                    isLoading={isLoading}
+                                    posts={posts}
+                                    pagination={pagination}
+                                />
                             </div>
                         </div>
                         <Sidebars/>
