f6c42ec05d19ad5926450e401b527f581834b44d
glapi <abhazim.nurbat@gmail.com>
2022-07-24 21:40:14 +0200

    Remove unused method

--- server/src/main/java/com/site/forum/dao/UserRepository.java
+++ server/src/main/java/com/site/forum/dao/UserRepository.java
@@ -8,16 +8,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     @Query("SELECT u FROM User u WHERE u.username LIKE %?1%")
     Page<User> searchUserByUsernameLike(String username, Pageable pageable);
-    @Query("SELECT h FROM User u LEFT JOIN u.hiddenPosts h WHERE u.username = ?1")
-    List<Post> getHiddenPosts(String username);
-    @Query("SELECT h FROM User u LEFT JOIN u.hiddenPosts h WHERE u.username = ?1")
+    @Query("SELECT h FROM User u LEFT JOIN u.hiddenPosts h WHERE u.username = :username")
     Page<Post> getHiddenPosts(String username, Pageable pageable);
 
 }
